1549a16e29e3b0fa83be87766c5f26cbe9355525
Eugen <glazov.it@gmail.com>
2018-04-09 10:41:33 +0400

    use commander.js for read args

---
+++ app/progr.js
@@ -0,0 +1,22 @@
+// app/progr.js
+var arg = require('commander')
+var calc = require('./calc')
+
+var numbersToAdd = [
+    3,
+    4,
+    10,
+    2
+]
+
+arg
+    .option('-t, --text', 'text output')
+    .parse(process.argv);
+
+var result = calc.sum(numbersToAdd)
+
+if (arg.text) {
+    console.log('The result is: nineteen')
+} else {
+    console.log('The result is:', result)
+}
